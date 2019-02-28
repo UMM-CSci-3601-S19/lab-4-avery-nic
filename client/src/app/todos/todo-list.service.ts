2d6ee6329846346130f872e458f6cddb67dfebef
robe1606 <robe1606@morris.umn.edu>
2019-02-28 13:17:10 -0600

    Implement filterByOwner functionality as well as removeParameter and parameterPresent functionality.

--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -9,8 +9,49 @@ import {environment} from '../../environments/environment';
 @Injectable()
 export class TodoListService {
   readonly baseUrl: string = environment.API_URL + 'todos';
-  private userUrl: string = this.baseUrl;
+  private todoUrl: string = this.baseUrl;
 
   constructor(private http: HttpClient) {
+  }
+
+  getTodos(todoOwner?: string): Observable<Todo[]> {
+    this.filterByOwner(todoOwner);
+    return this.http.get<Todo[]>(this.todoUrl);
+  }
+
+  getTodoById(id: string): Observable<Todo> {
+    return this.http.get<Todo>(this.todoUrl + '/' + id);
+  }
+
+  filterByOwner(todoOwner?: string): void {
+    if (this.parameterPresent('owner=')) {
+      // there was a previous search by owner that we need to clear
+      this.removeParameter('owner=');
+    }
+    if (!(todoOwner == null || todoOwner === '')) {
+      if (this.todoUrl.indexOf('?') !== -1) {
+        // there was already some information passed in this url
+        this.todoUrl += 'owner=' + todoOwner + '&';
+      } else {
+        // this was the first bit of information to pass in the url
+        this.todoUrl += '?owner=' + todoOwner + '&';
+      }
+    }
+  }
+
+  private parameterPresent(searchParam: string) {
+    return this.todoUrl.indexOf(searchParam) !== -1;
+  }
+
+  //remove the parameter and, if present, the &
+  private removeParameter(searchParam: string) {
+    let start = this.todoUrl.indexOf(searchParam);
+    let end = 0;
+    if (this.userUrl.indexOf('&') !== -1) {
+      end = this.todoUrl.indexOf('&', start) + 1;
+    } else {
+      end = this.todoUrl.indexOf('&', start);
+    }
+    this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end);
   }
 }

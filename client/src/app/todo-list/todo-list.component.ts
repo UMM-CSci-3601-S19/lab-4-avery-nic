4cc7fe3c5a38c0c2017908c3307b6a010bf39658
Avery Koranda <koran023@morris.umn.edu>
2019-02-28 17:08:31 -0600

    Write presence tests for todo-list.component

--- client/src/app/todo-list/todo-list.component.ts
+++ client/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-todo-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  public todos: Todo[];
 
   constructor() { }
 

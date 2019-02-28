4521e04b06abfa1f9479ad7aebaa7de54145eb94
Avery Koranda <koran023@morris.umn.edu>
2019-02-28 15:49:00 -0600

    Write getTodos() based on getUsers()

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,10 +1,13 @@
 package umm3601.todo;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class TodoController {
 
@@ -15,6 +18,24 @@ public class TodoController {
   }
 
   public String getTodos(Map<String, String[]> queryParams) {
-    return "";
+    Document filterDoc = new Document();
+
+    if (queryParams.containsKey("owner")) {
+      String requestedOwner = queryParams.get("owner")[0];
+      Document ownerQuery = new Document();
+      ownerQuery.append("$regex", requestedOwner);
+      ownerQuery.append("$options", "i");
+      filterDoc.append("owner", ownerQuery);
+    }
+
+    FindIterable<Document> matchingTodos = todoCollection.find(filterDoc);
+
+    return serializeIterable(matchingTodos);
+  }
+
+  private String serializeIterable(Iterable<Document> documents) {
+    return StreamSupport.stream(documents.spliterator(), false)
+      .map(Document::toJson)
+      .collect(Collectors.joining(", ", "[", "]"));
   }
 }

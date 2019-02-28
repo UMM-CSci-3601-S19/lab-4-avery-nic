d3f29141272169e9219502b43c37ec5debe3230e
Avery Koranda <koran023@morris.umn.edu>
2019-02-28 15:30:03 -0600

    Create setup method in TodoControllerSpec

---
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -0,0 +1,69 @@
+package umm3601.todo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.codecs.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for the TodoController.
+ * <p>
+ */
+
+public class TodoControllerSpec {
+  private TodoController todoController;
+  private ObjectId singleTodoId;
+
+  @Before
+  public void clearAndPopulateDB() {
+    MongoClient mongoClient = new MongoClient();
+    MongoDatabase db = mongoClient.getDatabase("test");
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Chris\",\n" +
+      "                    body: This is Chris's task,\n" +
+      "                    status: true,\n" +
+      "                    category: \"software design\"\n" +
+      "                }"));
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Pat\",\n" +
+      "                    body: This is Pat's todo,\n" +
+      "                    status: false,\n" +
+      "                    category: \"video games\"\n" +
+      "                }"));
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Jamie\",\n" +
+      "                    body: This is Jamie's todo,\n" +
+      "                    status: true,\n" +
+      "                    category: \"groceries\"\n" +
+      "                }"));
+
+    singleTodoId = new ObjectId();
+    BasicDBObject singleTodo = new BasicDBObject("_id", singleTodoId);
+    singleTodo = singleTodo.append("owner", "Geoffrey")
+      .append("body", "This is Geoffrey's todo")
+      .append("status", false)
+      .append("category", "fishing");
+
+    todoDocuments.insertMany(testTodos);
+    todoDocuments.insertOne(Document.parse(singleTodo.toJson()));
+
+    todoController = new TodoController(db);
+  }
+
+}

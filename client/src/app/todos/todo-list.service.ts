8f22f063040a11c142995339c091fbdf1e76ae23
robe1606 <robe1606@morris.umn.edu>
2019-02-28 14:09:10 -0600

    Copy and modify tests from user-list.service.spec.ts

--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -24,17 +24,27 @@ export class TodoListService {
   }
 
   filterByOwner(todoOwner?: string): void {
-    if (this.parameterPresent('owner=')) {
-      // there was a previous search by owner that we need to clear
-      this.removeParameter('owner=');
-    }
     if (!(todoOwner == null || todoOwner === '')) {
+      if (this.parameterPresent('owner=')) {
+        // there was a previous search by owner that we need to clear
+        this.removeParameter('owner=');
+      }
       if (this.todoUrl.indexOf('?') !== -1) {
         // there was already some information passed in this url
         this.todoUrl += 'owner=' + todoOwner + '&';
       } else {
         // this was the first bit of information to pass in the url
         this.todoUrl += '?owner=' + todoOwner + '&';
+      }
+    } else {
+      // there was nothing in the box to put onto the URL... reset
+      if (this.parameterPresent('owner=')) {
+        let start = this.todoUrl.indexOf('owner=');
+        const end = this.todoUrl.indexOf('&', start);
+        if (this.todoUrl.substring(start - 1, start) === '?') {
+          start = start - 1;
+        }
+        this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end + 1);
       }
     }
   }
@@ -47,7 +57,7 @@ export class TodoListService {
   private removeParameter(searchParam: string) {
     let start = this.todoUrl.indexOf(searchParam);
     let end = 0;
-    if (this.userUrl.indexOf('&') !== -1) {
+    if (this.todoUrl.indexOf('&') !== -1) {
       end = this.todoUrl.indexOf('&', start) + 1;
     } else {
       end = this.todoUrl.indexOf('&', start);

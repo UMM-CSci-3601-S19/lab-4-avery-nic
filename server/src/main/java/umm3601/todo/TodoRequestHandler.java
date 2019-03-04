830989abfefb8a5da56f64adb9334ab77ff49038
Avery Koranda <koran023@morris.umn.edu>
2019-03-03 20:07:25 -0600

    Write addNewTodo for TodoRequestHandler
    Based on UserRequestHandler.

--- server/src/main/java/umm3601/todo/TodoRequestHandler.java
+++ server/src/main/java/umm3601/todo/TodoRequestHandler.java
@@ -16,4 +16,21 @@ public class TodoRequestHandler {
     res.type("application/json");
     return todoController.getTodos(req.queryMap().toMap());
   }
+
+  public String addNewTodo(Request req, Response res) {
+    res.type("application/json");
+
+    Document newTodo = Document.parse(req.body());
+
+    String owner = newTodo.getString("owner");
+    String body = newTodo.getString("body");
+    boolean status = newTodo.getBoolean("status");
+    String category = newTodo.getString("category");
+
+    System.err.println("Adding new todo [owner=" + owner
+      + ", body=" + body
+      + " status=" + status
+      + " category=" + category + ']');
+    return todoController.addNewTodo(owner, body, status, category);
+  }
 }
